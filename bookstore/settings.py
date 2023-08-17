0d923ede753b45d23daa15731cd75d0b5fe8b4be
migrice <efomenakuete@gmail.com>
2023-08-17 10:09:48 +0100

    storage in settings

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -183,7 +183,9 @@ STATICFILES_FINDERS = [
 ]
 
 STORAGES = {
-    # ...
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
